83f37dba72312767c204f9bc6aaa77961e970107
BenJarrett <b.cl.jarrett@gmail.com>
2023-12-09 16:02:57 -0500

    created physics entit. Left and right movement for player with a and d

--- game.py
+++ game.py
@@ -2,6 +2,9 @@ import sys
 
 import pygame
 
+from scripts.utils import load_image
+from scripts.entities import PhysicsEntity
+
 
 class Game:
     def __init__(self):
@@ -12,55 +15,43 @@ class Game:
 
         self.clock = pygame.time.Clock()
 
-        self.img = pygame.image.load("data/images/clouds/cloud_1.png")
-        self.img.set_colorkey((0, 0, 0))
-
-        self.img_pos = [160, 260, 0, 0]
         self.movement = [False, False, False, False]
 
-        self.collision_area = pygame.Rect(50, 50, 300, 50)
+        self.assets ={
+            "player": load_image("entities/player.png"),
+        }
+
+        self.player = PhysicsEntity(self, "player", (50, 50), (8, 15))
 
     def run(self):
         while True:
             self.screen.fill((14, 219, 248))
 
-            img_r = pygame.Rect(
-                self.img_pos[0],
-                self.img_pos[1],
-                self.img.get_width(),
-                self.img.get_height(),
-            )
-            if img_r.colliderect(self.collision_area):
-                pygame.draw.rect(self.screen, (0, 100, 255), self.collision_area)
-            else:
-                pygame.draw.rect(self.screen, (0, 50, 155), self.collision_area)
-
-            self.img_pos[1] += (self.movement[1] - self.movement[0]) * 3
-            self.img_pos[0] -= (self.movement[2] - self.movement[3]) * 2.5
-            self.screen.blit(self.img, self.img_pos)
+            self.player.update((self.movement[1] - self.movement[0], 0))
+            self.player.render(self.screen)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_w:
+                    if event.key == pygame.K_a:
                         self.movement[0] = True
-                    if event.key == pygame.K_s:
+                    if event.key == pygame.K_d:
                         self.movement[1] = True
-                    if event.key == pygame.K_a:
-                        self.movement[2] = True
-                    if event.key == pygame.K_d:
-                        self.movement[3] = True
+                    # if event.key == pygame.K_a:
+                    #     self.movement[2] = True
+                    # if event.key == pygame.K_d:
+                    #     self.movement[3] = True
                 if event.type == pygame.KEYUP:
-                    if event.key == pygame.K_w:
-                        self.movement[0] = False
-                    if event.key == pygame.K_s:
-                        self.movement[1] = False
                     if event.key == pygame.K_a:
-                        self.movement[2] = False
+                        self.movement[0] = False
                     if event.key == pygame.K_d:
-                        self.movement[3] = False
+                        self.movement[1] = False
+                    # if event.key == pygame.K_a:
+                    #     self.movement[2] = False
+                    # if event.key == pygame.K_d:
+                    #     self.movement[3] = False
 
             pygame.display.update()
             self.clock.tick(60)
---
+++ scripts/entities.py
@@ -0,0 +1,24 @@
+import pygame
+
+
+class PhysicsEntity:
+    def __init__(self, game, e_type, pos, size):
+        self.game = game
+        self.type = e_type
+        self.pos = list(pos)
+        self.size = size
+        self.velocity = [0, 0]
+        # self.acceleration = [0, 0]
+        # self.rect = pygame.Rect(self.pos[0], self.pos[1], self.size[0], self.size[1])
+
+    def update(self, movement=(0, 0)):
+        frame_movement = (
+            movement[0] + self.velocity[0],
+            movement[1] + self.velocity[1]
+        )
+
+        self.pos[0] += frame_movement[0]
+        self.pos[1] += frame_movement[1]
+
+    def render(self, surface):
+        surface.blit(self.game.assets["player"], self.pos)
